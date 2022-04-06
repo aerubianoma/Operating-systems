1995c21d125325216283f09b5901f4b82a1a55a4
aerubianoma <aerubianoma@unal.edu.co>
2022-04-05 20:02:08 -0500

    firts commit
    Here is the menu for the lab 1 work

---
+++ Lab1/README.txt
@@ -0,0 +1,4 @@
+Integrantes:
+- Andres Eduardo Rubiano Martinez: aerubianoma@unal.edu.co
+- Santiago Tovar Mosquera: satovarmo@unal.edu.co
+- Diego ... : ...
---
+++ Lab1/menu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int menu() {
+
+	printf("Bienvenido \n");
+	printf("1. Ingresar Origen \n");
+	printf("2. Ingresar destino \n");
+	printf("3. Ingresar hora \n");
+	printf("4. Buscar tiempo de viaje medio \n");
+	printf("5. salir \n");
+
+return 0;
+}
+
+int main() {
+	
+	int menu();
+	menu();
+return 0;
+}
