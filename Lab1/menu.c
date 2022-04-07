d1eec7b95210ce54621d3f73b8e2bca4e20c4fe8
Santiago Tovar <satovarmo@unal.edu.co>
2022-04-06 19:51:15 -0500

    Menu con switch-case

--- Lab1/menu.c
+++ Lab1/menu.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int menu() {
-
+	int entrada;
 	printf("Bienvenido \n");
 	printf("1. Ingresar Origen \n");
 	printf("2. Ingresar destino \n");
 	printf("3. Ingresar hora \n");
 	printf("4. Buscar tiempo de viaje medio \n");
 	printf("5. salir \n");
-
-return 0;
+	scanf("%d", &entrada);
+return entrada;
 }
 
 int main() {
-	
-	int menu();
-	menu();
+	int a=menu();
+	switch(a){
+		case 1:
+			printf("Ingresará el origen \n");
+		break;
+		case 2:
+			printf("Ingresará el destino \n");
+		break;
+		case 3:
+			printf("Ingresará la hora \n");
+		break;
+		case 4:
+			printf("Buscará el tiempo \n");
+		break;
+		case 5:
+			printf("Salió \n");
+		break;
+	}
 return 0;
 }
