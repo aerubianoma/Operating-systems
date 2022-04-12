326fe6bbf93e0c7e9dbc06cff8ee22550f9da0ea
Santiago Tovar <satovarmo@unal.edu.co>
2022-04-12 12:00:02 -0500

    Menufull

--- Lab1/menu.c
+++ Lab1/menu.c
@@ -4,7 +4,6 @@
 
 int menu() {
 	int entrada;
-	printf("Bienvenido \n");
 	printf("1. Ingresar Origen \n");
 	printf("2. Ingresar destino \n");
 	printf("3. Ingresar hora \n");
@@ -15,23 +14,44 @@ return entrada;
 }
 
 int main() {
+	printf("Bienvenido \n");
 	int a=menu();
-	switch(a){
-		case 1:
-			printf("Ingresará el origen \n");
-		break;
-		case 2:
-			printf("Ingresará el destino \n");
-		break;
-		case 3:
-			printf("Ingresará la hora \n");
-		break;
-		case 4:
-			printf("Buscará el tiempo \n");
-		break;
-		case 5:
-			printf("Salió \n");
-		break;
+	int IDorig=0,IDFinal=0,Hora=24;
+	while(a!=5){
+		switch(a){
+			case 1:
+				while(1>IDorig || IDorig>1160){
+				printf("Ingrese el origen\n");
+				scanf("%d",&IDorig);
+				}
+				a=menu();
+			break;
+			case 2:
+				while(1>IDFinal || IDFinal>1160){
+				printf("Ingrese el final\n");
+				scanf("%d",&IDFinal);
+				}
+				a=menu();
+			break;
+			case 3:
+				while(0>Hora || Hora>23){
+				printf("Ingrese la hora\n");
+				scanf("%d",&Hora);
+				}
+				a=menu();
+			break;
+			case 4:
+				printf("Buscará el tiempo \n");
+				a=menu();
+			break;
+			case 5:
+				printf("Salió \n");
+			break;
+			default:
+				printf("Seleccione una opciòn vàlida\n");
+				a=menu();
+			break;
+		}
 	}
 return 0;
 }
